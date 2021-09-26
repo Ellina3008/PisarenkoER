253878b05870f4197f8ba9b257818e5c45510b75
Ellina3008 <91068291+Ellina3008@users.noreply.github.com>
2021-09-26 22:36:21 +0300

    Add files via upload

---
+++ 1 урок/Untitled-6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#6 Задание:
+print("6 Задание: ")
+
+x = 8
+y = 6
+print(x, y)
+x, y = y, x
+print(x, y)
---
+++ 1 урок/Untitled-7.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#7 Задание:
+print("7 Задание: ")
+
+number = int(input("Введите число: "))
+if number % 2 == 0:
+        print('Четное число')
+else:
+     print('Нечетное число')       
